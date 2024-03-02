9afbb6f377b481b738458945a2fea1946d1018b9
Nguyenduc2108 <vanducnguyen2108@gmail.com>
2024-03-02 15:42:19 +0700

    update todolist 15:41 3/2/2024

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { useState } from "react";
+
 function App() {
-  return <div className="App">app</div>;
+  const [work, setWork] = useState("");
+  console.log(work);
+  return (
+    <div className="h-screen flex gap-8 justify-center border border-red-500 items-center">
+      <input
+        type="text"
+        className="outline-none  border border-blue-600 px-4 py-2 w-[400px]"
+        value={work}
+        onChange={(e) => setWork(e.target.value)}
+      />
+      <button
+        type="button"
+        className="outline-none px-4 py-2 bg-blue-500 rounded-md text-white"
+      >
+        Add
+      </button>
+    </div>
+  );
 }
 
 export default App;
